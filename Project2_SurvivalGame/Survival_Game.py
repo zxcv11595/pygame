014d475fb447aa87dc2cae16f4265729da1e155e
lee <zxcv11595@gmail.com>
2023-11-17 02:37:25 +0900

    update animation

--- Project2_SurvivalGame/Survival_Game.py
+++ Project2_SurvivalGame/Survival_Game.py
@@ -2,144 +2,342 @@ import pygame
 import sys
 import random
 
-#초기화
 pygame.init()
 
-#화면 크기 및 색상 설정
+clock = pygame.time.Clock()
+fps = 60
+
+#game window
 WIDTH, HEIGHT = 800, 600
-window = pygame.display.set_mode((WIDTH, HEIGHT))
+screen = window = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Survival Game")
 
-#색깔
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
+#Color
+Red = (255, 0, 0)
+Green = (0, 255, 0)
+Blue = (0, 0, 255)
+White = (255, 255, 255)
 
-#프레임
-framerate = 100
+#Drawing background
+Background_img = pygame.image.load('img/brick_02.png').convert_alpha()
 
-#Player 클래스
+def Draw_Background():
+    screen.blit(Background_img, (0, 0))
+
+#Player class
 class Player:
-    def __init__(self):
-        self.x, self.y = WIDTH//2, HEIGHT//2
+    def __init__(self, x, y, name, max_hp, exp, score, level, damage):
+        self.x, self.y = x, y
         self.width = 10
         self.height = 10
-        self.velocity = 5
+        self.velocity = 2
+        self.name = name
+        self.max_hp = max_hp
+        self.hp = max_hp
+        self.exp = exp
+        self.max_exp = exp
+        self.level = level
+        self.score = score
+        self.damage = damage
+        self.direction = 'right'
+        self.alive = True
+        self.animation_list = []
+        self.frame_index = 0
+        self.action = 0 #0:idle, 1:move, 2:attack, 3:hurt, 4:dead
+        self.update_time = pygame.time.get_ticks()
+        #load idle images
+        temp_list = []
+        for i in range(1, 5):
+            img = pygame.image.load(f'img/{self.name}/idle_{i}.png')
+            img = pygame.transform.scale(img, (img.get_width()*0.2, img.get_height()*0.2))
+            temp_list.append(img)
+        self.animation_list.append(temp_list)
+        #load move images
+        temp_list = []
+        for i in range(1, 5):
+            img = pygame.image.load(f'img/{self.name}/run_{i}.png')
+            img = pygame.transform.scale(img, (img.get_width()*0.2, img.get_height()*0.2))
+            temp_list.append(img)
+        self.animation_list.append(temp_list)
+        #load attack images
+        temp_list = []
+        for i in range(1, 4):
+            img = pygame.image.load(f'img/{self.name}/attack_{i}.png')
+            img = pygame.transform.scale(img, (img.get_width()*0.2, img.get_height()*0.2))
+            temp_list.append(img)
+        self.animation_list.append(temp_list)
+        #load hurt images
+        temp_list = []
+        for i in range(1, 4):
+            img = pygame.image.load(f'img/{self.name}/hurt_{i}.png')
+            img = pygame.transform.scale(img, (img.get_width()*0.2, img.get_height()*0.2))
+            temp_list.append(img)
+        self.animation_list.append(temp_list)
+        #load dead images
+        temp_list = []
+        for i in range(1, 5):
+            img = pygame.image.load(f'img/{self.name}/dead_{i}.png')
+            img = pygame.transform.scale(img, (img.get_width()*0.2, img.get_height()*0.2))
+            temp_list.append(img)
+        self.animation_list.append(temp_list)
+        self.image = self.animation_list[self.action][self.frame_index]
+        self.rect = self.image.get_rect()
 
     def move(self, direction):
-        if direction == "left" and self.x - self.velocity > 0:
-            self.x -= self.velocity
-        elif direction == "right" and self.x + self.velocity < WIDTH - self.width:
-            self.x += self.velocity
-        elif direction == "up" and self.y - self.velocity > 0: 
-            self.y -= self.velocity
-        elif direction == "down" and self.y + self.velocity < WIDTH - self.height:
-            self.y += self.velocity
+        if self.action not in (2, 3):  # 공격 또는 피해 애니메이션이 동작 중이 아닌 경우에만 움직임 처리
+            if direction == "left" and self.x - self.velocity > 0:
+                self.x -= self.velocity
+                self.direction = "left"
+                self.action = 1
+            elif direction == "right" and self.x + self.velocity < WIDTH - self.width:
+                self.x += self.velocity
+                self.direction = "right"
+                self.action = 1
+            elif direction == "up" and self.y - self.velocity > 0:
+                self.y -= self.velocity
+                self.action = 1
+            elif direction == "down" and self.y + self.velocity < HEIGHT - self.height:
+                self.y += self.velocity
+                self.action = 1
+            else:
+                self.action = 0
+    
+    def attack(self):
+        self.frame_index = 0
+        self.action = 2
+        self.update_time = pygame.time.get_ticks()
+        
+        
+    def hurt(self):
+        self.action = 3
+        self.frame_index = 0
+        self.update_time = pygame.time.get_ticks()
 
-#Enemy 클래스
-class Enemy:
-    def __init__(self):
-        self.x = random.randint(0, WIDTH)
-        self.y = random.randint(0, HEIGHT)
-        self.width = 10
-        self.height = 10
-        self.velocity = 3
+    def update(self):
+        animation_cooldown = 100
+        self.image = self.animation_list[self.action][self.frame_index]
+        if pygame.time.get_ticks() - self.update_time > animation_cooldown:
+            self.update_time = pygame.time.get_ticks()
+            self.frame_index += 1
+        if self.frame_index >= len(self.animation_list[self.action]) - 1:
+            self.frame_index = 0
+            if self.action == 1:
+                self.action = 0
+            if self.action == 2:
+                self.action = 0
 
+    def draw(self):
+        self.rect.center = (self.x, self.y)
+        if self.direction == "right":
+            screen.blit(self.image, self.rect)
+        else:
+            screen.blit(pygame.transform.flip(self.image, True, False), self.rect)
+
+#Monster class
+class Monster:
+    def __init__(self, x, y, name ,hp, damage, attack_range):
+        self.x, self.y = x, y
+        self.velocity = 1
+        self.name = name
+        self.hp = hp
+        self.damage = damage
+        self.attack_range = attack_range
+        self.direction = 'right'
+        self.alive = True
+        self.animation_list = []
+        self.frame_index = 0
+        self.action = 0 #0:idle, 1:move, 2:attack, 3:hurt, 4:dead
+        self.update_time = pygame.time.get_ticks()
+        #load images
+        temp_list = []
+        for i in range(1, 4):
+            img = pygame.image.load(f'img/{self.name}/Idle{i}.png')
+            temp_list.append(img)
+        self.animation_list.append(temp_list)
+        temp_list = []
+        #load move images
+        temp_list = []
+        for i in range(1, 7):
+            img = pygame.image.load(f'img/{self.name}/Walk{i}.png')
+            temp_list.append(img)
+        self.animation_list.append(temp_list)
+        #load attack images
+        temp_list = []
+        for i in range(1, 6):
+            img = pygame.image.load(f'img/{self.name}/Attack{i}.png')
+            temp_list.append(img)
+        self.animation_list.append(temp_list)
+        #load hurt images
+        temp_list = []
+        for i in range(1, 3):
+            img = pygame.image.load(f'img/{self.name}/Hurt{i}.png')
+            temp_list.append(img)
+        self.animation_list.append(temp_list)
+        #load dead images
+        temp_list = []
+        for i in range(1, 7):
+            img = pygame.image.load(f'img/{self.name}/Death{i}.png')
+            temp_list.append(img)
+        self.animation_list.append(temp_list)
+        self.image = self.animation_list[self.action][self.frame_index]
+        self.rect = self.image.get_rect()
+    
     def move_towards(self, target_x, target_y):
-        if target_x > self.x:
-            self.x += self.velocity
+        if self.action not in (2, 3):
+            distance = ((self.x - target_x) ** 2 + (self.y - target_y) ** 2) ** 0.5
+            if distance <= self.attack_range:
+                self.attack()
+            elif target_x > self.x:
+                self.x += self.velocity
+                self.direction = 'right'
+                self.action = 1
+            else:
+                self.x -= self.velocity
+                self.direction = 'left'
+                self.action = 1
+            
+            if distance > self.attack_range:
+                if target_y > self.y:
+                    self.y += self.velocity
+                    self.action = 1
+                else:
+                    self.y -= self.velocity
+                    self.action = 1
+
+    def attack(self):
+        self.frame_index = 0
+        self.action = 2
+        self.update_time = pygame.time.get_ticks()
+        if len(self.animation_list) > 2:
+            self.animation_list.pop()  # Remove any extra animations
+        temp_list = []
+        for i in range(1, 6):
+            img = pygame.image.load(f'img/{self.name}/Attack{i}.png')
+            temp_list.append(img)
+        self.animation_list.append(temp_list)
+        self.image = self.animation_list[self.action][self.frame_index]
+        self.rect = self.image.get_rect()
+
+    def hurt(self):
+        self.action = 3
+        self.frame_index = 0
+        self.update_time = pygame.time.get_ticks()
+
+    def dead(self):
+        if self.alive == False:
+            self.action = 4
+            self.frame_index = 0
+            self.update_time = pygame.time.get_ticks()
+
+    def update(self):
+        animation_cooldown = 100
+        self.image = self.animation_list[self.action][self.frame_index]
+        if pygame.time.get_ticks() - self.update_time > animation_cooldown:
+            self.update_time = pygame.time.get_ticks()
+            self.frame_index += 1
+        if self.frame_index >= len(self.animation_list[self.action]) - 1:
+            self.frame_index = 0
+            if self.action == 1:
+                self.action = 0
+            if self.action == 2:
+                self.action = 0
+    
+    def draw(self):
+        if self.alive:
+            self.rect.center = (self.x, self.y)
+            if self.direction == "right":
+                screen.blit(self.image, self.rect)
+            else:
+                screen.blit(pygame.transform.flip(self.image, True, False), self.rect)
         else:
-            self.x -= self.velocity
+            self.dead()
+            if self.frame_index >= len(self.animation_list[self.action]) - 1:
+                Monster.remove(self)
 
-        if target_y > self.y:
-            self.y += self.velocity
-        else:
-            self.y -= self.velocity
+#Player stat class
+class stat_bar:
+    def __init__(self, x, y, hp, max_hp, exp, max_exp, level, score):
+        self.x = x
+        self.y = y
+        self.hp = hp
+        self.max_hp = max_hp
+        self.exp = exp
+        self.max_exp = max_exp
+        self.score = score
+        self.level = level
 
-player = Player()
-enemy = Enemy()
+    def text(self, score, level):
+        pygame.draw.rect(screen, Blue, (self.x, self.y, 800, 40))
+        score_text = pygame.font.SysFont('comicsans', 30).render("Score: " + str(score), True, White)
+        level_text = pygame.font.SysFont('comicsans', 30).render("level: " + str(level), True, White)
+        window.blit(score_text, [10, 0])
+        window.blit(level_text, [680, 0])
+    
+    def draw(self, hp, exp):
+        self.hp = hp
+        ratio = self.hp / self.max_hp
+        self.exp = exp
+        pygame.draw.rect(screen, Red, (0, 560, 800, 20))
+        pygame.draw.rect(screen, Green, (0, 560, 800 * ratio, 20))
+        pygame.draw.rect(screen, White, (0, 580, 800, 20))
+        pygame.draw.rect(screen, Blue, (0, 580, 800, 20))
 
-#체력, 점수, 경험치, 레벨
-score = 0
-hp = 100
-level = 1
-exp = 0
+Wizard = Player(400, 300, 'Wizard', 10, 0, 0, 1, 1)
 
+Lizard1 = Monster(600, 400, 'Lizard', 1, 1, 50)
+Lizard2 = Monster(700, 200, 'Lizard', 1, 1, 50)
+Lizard_list = []
+Lizard_list.append(Lizard1)
+Lizard_list.append(Lizard2)
 
-#게임 종료 후 점수 표시
-def Display_score(score):
-    value = pygame.font.SysFont('comicsans', 30).render("Your Score: " + str(score), True, BLACK)
-    window.blit(value, [0, 0])
+Wizard_stat = stat_bar(0, 0, Wizard.hp, Wizard.max_hp, Wizard.exp, Wizard.max_exp, Wizard.level, Wizard.score)
 
-#체력, 레벨, 경험치 표시
-def Display_info(hp, level, exp):
-    hp_text = pygame.font.SysFont('comicsans', 30).render("HP: " + str(hp), True, BLACK)
-    level_text = pygame.font.SysFont('comicsans', 30).render("Level: " + str(level), True, BLACK)
-    exp_text = pygame.font.SysFont('comicsans', 30).render("exp: " + str(exp), True, BLACK)
-    window.blit(hp_text, [10, 10])
-    window.blit(level_text, [10, 40])
-    window.blit(exp_text, [10, 70])
-
-#메인 함수
 def gameLoop():
-    global player, enemy
-    global score, hp, level, exp
-
-     #게임 종료
     game_over = False
     game_close = False
 
-    while not game_over: 
-        while game_close == True:
-            window.fill(WHITE)
-            Display_score(score)
-            pygame.display.update()
-
-        #재도전
+    while not game_over:
         for event in pygame.event.get():
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_q:
-                    game_over = True
-                    game_close = False
-                if event.key == pygame.K_c:
-                    gameLoop()
+            if event.type == pygame.QUIT:
+                game_over = True
 
-        #player 이동
+        # 키 입력 처리
         keys = pygame.key.get_pressed()
         if keys[pygame.K_LEFT]:
-            player.move("left")
+            Wizard.move("left")
         if keys[pygame.K_RIGHT]:
-            player.move("right")
+            Wizard.move("right")
         if keys[pygame.K_UP]:
-            player.move("up")
+            Wizard.move("up")
         if keys[pygame.K_DOWN]:
-            player.move("down")            
+            Wizard.move("down")
+        if keys[pygame.K_SPACE]:
+            Wizard.attack()
 
-        #enemy가 palyer 추격
-        if not game_over:
-            enemy.move_towards(player.x, player.y)
+        pygame.draw.circle(screen, (255, 255, 255), (int(Wizard.x), int(Wizard.y)), 5)    
 
-        #충돌 검사
-        if abs(player.x - enemy.x) < player.width and abs(player.y - enemy.y) < player.height:
-            print("Game Over!")
-            game_close = True
+        # 캐릭터 및 적 업데이트 및 그리기
+        #Draw_Background()
+        Wizard.update()
+        Wizard.draw()
+        Wizard_stat.draw(Wizard.hp, Wizard.exp)
+        Wizard_stat.text(Wizard.score, Wizard.level)
 
-        #화면 초기화
-        window.fill(WHITE)
+        for Lizard in Lizard_list:
+            Lizard.update()
+            Lizard.draw()
+            Lizard.move_towards(Wizard.x, Wizard.y)
 
-        #player 그리기
-        pygame.draw.rect(window, BLUE, (player.x, player.y, player.width, player.height))
 
-        #enemy 그리기
-        pygame.draw.rect(window, RED, (enemy.x, enemy.y, enemy.width, enemy.height))
-
-        #프레임 100Hz 제한
-        pygame.time.Clock().tick(framerate)    
-
-        Display_info(hp, level, exp)
         pygame.display.update()
+
+        clock.tick(fps)
+
+        pygame.draw.circle(screen, (255, 0, 0), (int(Wizard.x), int(Wizard.y)), 10)
+
+        # 몬스터 위치를 표시하는 빨간색 점 그리기
+        for Lizard in Lizard_list:
+            pygame.draw.circle(screen, (255, 0, 0), (int(Lizard.x), int(Lizard.y)), 10)
 
     pygame.quit()
     quit()
